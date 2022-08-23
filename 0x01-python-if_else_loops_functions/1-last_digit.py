5c95509b5f14d9d25e45296f1bc0bc02d8012da0
ianmax32 <masagaian32@gmail.com>
2022-08-23 13:25:33 -0700

    Tha last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,11 @@ lastdigit = 0
 if number < 0:
     number *= -1
     lastdigit = number % 10
-lastdigit = number % 10
-print("Last digit of {} is {} and is ".format(number,lastdigit), end="")
+    number *= -1
+    lastdigit *= -1
+else:
+    lastdigit = number % 10
+print("Last digit of {} is {} and is ".format(number, lastdigit), end="")
 if lastdigit > 5:
     print("greater than 5")
 elif lastdigit == 0:
