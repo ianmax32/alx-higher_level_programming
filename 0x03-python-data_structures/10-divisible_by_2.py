0373ce99b9fb3dcb41f39fcfce0bb32e602443ce
ianmax32 <masagaian32@gmail.com>
2022-08-29 19:54:07 -0700

    Only by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,8 @@ def divisible_by_2(my_list=[]):
     new_list = []
     if not my_list or my_list is None:
         return (None)
-    for i in my_list:
+    new_list = list(my_list)
+    for i in new_list:
         if not i % 2:
             new_list[i] = True
         else:
