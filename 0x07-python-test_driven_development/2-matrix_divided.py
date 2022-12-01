067ee5f7e24e63453406cdaf0d615c8895f20b23
ianmax32 <masagaian32@gmail.com>
2022-11-30 19:36:14 -0800

    0. First constant, first print

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Module for matrix devided function"""
+
+def matrix_divided(matrix, div):
+    """this is a function for deviding all
+    elements of a matrix"""
+
+    try:
+        x = a + 10
+        y = b + 10
+    except TypeError:
+        if not y:
+            return "b must be an integer"
+        if not x:
+            return "a must be an integer"
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
