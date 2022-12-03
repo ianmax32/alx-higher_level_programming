65b5684838d163f846278a84dc00b29c613f06f1
ianmax32 <masagaian32@gmail.com>
2022-12-03 01:53:06 -0800

    I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+if(parseInt(process.argv[2]))
+{
+	for(let x = 0; x < process.argv[2]; x++)
+	{
+		console.log('C is fun');
+	}
+}
+else
+{
+	console.log('Missing number of occurrence');
+}
