c2186af928d59a38b23676a3749d742582cb3d1d
ianmax32 <masagaian32@gmail.com>
2022-12-03 01:48:34 -0800

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,9 +2,7 @@
 
 const values = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-let a = 0;
-
-for(a; a < values.length; a++)
+for(let a = 0; a < values.length; a++)
 {
 	console.log(values[a]);
 }
