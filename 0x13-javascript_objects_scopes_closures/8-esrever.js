a8a7328cafc98d569dfbc30504af359546f80b6a
ianmax32 <masagaian32@gmail.com>
2022-12-03 06:18:59 -0800

    Esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 
 exports.esrever = function (list) {
   const values = [];
-  for (let a = list.length - 1; a >= 0; a++) {
+  for (let a = list.length - 1; a >= 0; a--) {
     values.push(list[a]);
   }
   return values;
