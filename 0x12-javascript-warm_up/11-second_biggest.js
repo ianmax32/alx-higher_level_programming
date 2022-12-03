5c14c86a250b1cb509fb491a84a07c6330ee9043
ianmax32 <masagaian32@gmail.com>
2022-12-03 03:45:18 -0800

    Object

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let values = [];
+if(arguments.length === 1)
+{
+	console.log('0');
+}
+else
+{
+	values.append(Array.prototype.slice(arguments).sort().reverse());
+	console.log(values[1]);
+}
