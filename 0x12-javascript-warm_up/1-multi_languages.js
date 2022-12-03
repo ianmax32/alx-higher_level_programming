befb4abbba522db2cb0970b2bfe570c2250e1ad9
ianmax32 <masagaian32@gmail.com>
2022-12-03 04:24:53 -0800

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
+// this script prints three lines:
 
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
