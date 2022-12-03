73259a6769e06f5b9f44be783bd20edfac7d3a55
ianmax32 <masagaian32@gmail.com>
2022-12-03 02:28:30 -0800

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const a= process.argv[2];
+
+function factorial(a)
+{
+	if(a === 0)
+	{
+		 return 1;
+	}
+	else if(process.argv.length === 2)
+	{
+		return 1;
+	}
+	else
+	{
+		return a * factorial(a - 1);
+	}
+}
+
+console.log(factorial(a));
