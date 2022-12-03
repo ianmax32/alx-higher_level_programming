494128125f8b3ebf0b6a734bb19e39ffa275893e
ianmax32 <masagaian32@gmail.com>
2022-12-03 06:42:22 -0800

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """this module reads a text file and prints it to stdout"""
 
+
 def read_file(filename=""):
     """this function is opening and reading the file"""
     with open(filename, encoding='utf-8') as rd_file:
