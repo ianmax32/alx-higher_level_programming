9fdd120234ed9fab2cc20e4cfe0b1bff6a621b94
ianmax32 <masagaian32@gmail.com>
2022-12-03 07:05:43 -0800

    Append to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+this module writes a string to a text file and
+returns the number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """this opens and writes to the file"""
+    a = 0
+    with open(filename, 'a') as wr_file:
+        for i in text:
+            wr_file.write(i)
+            a += 1
+    return a
