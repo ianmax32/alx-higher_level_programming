5ae2eddb7cce0c91ea0becba53d62dd9ce10c28a
ianmax32 <masagaian32@gmail.com>
2022-12-03 16:41:17 -0800

    Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""this module adds all arguments to a python
+list and save then to a file
+"""
+
+
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+
