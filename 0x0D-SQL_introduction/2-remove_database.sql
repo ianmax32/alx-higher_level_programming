95e3ecd1c911acc1de9914dc5195cfb5abb7acc2
ianmax32 <masagaian32@gmail.com>
2022-12-03 18:20:27 -0800

    List by best

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- this script deletes the database hbtn_0c_0 in the myql server
+-- If the database hbtn_0c_0 doesn’t exist, your script should not fail
+DROP DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- this script creates a table called first_table in the current database in your MySQL server
+-- The database name will be passed as an argument of the mysql command
+CREATE TABLE IF NOT EXISTS first_table(
+	id int,
+	name VARCHAR(256)
+);
