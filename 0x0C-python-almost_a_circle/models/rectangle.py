a4cf34130a223b55c0a3a619cc1537a5c60dc3b7
ianmax32 <masagaian32@gmail.com>
2022-12-03 17:22:11 -0800

    First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,7 +14,7 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """constructor"""
         super().__init__(id)
-        self.width = Rectangle.__width
-        self.height = Rectangle.__height
-        self.x = Rectangle.__x
-        self.y = Rectangle.__y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y 
