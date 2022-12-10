bc100bdab2722456235585403b5d36c3f2c02b70
ianmax32 <masagaian32@gmail.com>
2022-12-10 14:40:41 -0800

    Filter states

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+
+
