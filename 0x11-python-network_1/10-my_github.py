537a00ff4473a40dc7c16120ece80b966485288f
ianmax32 <masagaian32@gmail.com>
2022-12-16 18:40:03 -0800

    My Github!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,18 @@
-#!/usr/bin/python3\n# this script
+#!/usr/bin/python3
+"""
+this script takes your GitHub credentials (username and password) 
+and uses the GitHub API to display your id
+"""
+
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        r = requests.get('https://api.github.com/user',
+                         auth=(argv[1], argv[2])).json()
+        print(r.get('id'))
+    except:
+        print('None')
