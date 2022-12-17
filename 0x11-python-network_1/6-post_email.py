e4dc1cc6c181945850405bd149dd76389a9854e7
ianmax32 <masagaian32@gmail.com>
2022-12-16 17:34:04 -0800

    Post an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,15 @@
-#!/usr/bin/python3\n# this script
+#!/usr/bin/python3
+"""
+this script takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter, and displays the body of the response
+(decoded in utf-8)
+"""
+
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    r = requests.post(argv[1], email={'email': argv[2]}).text
+    print(r)
