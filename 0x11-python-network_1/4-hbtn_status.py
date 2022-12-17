d26dce54940824afabf5a0e31f59988d63747814
ianmax32 <masagaian32@gmail.com>
2022-12-16 17:23:04 -0800

    What's my status #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,15 @@
-#!/usr/bin/python3\n# this script
+#!/usr/bin/python3
+"""
+this script fetches https://alx-intranet.hbtn.io/status
+using the package requests
+"""
+
+
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(r)))
+    print('\t- content: {}'.format(r))
