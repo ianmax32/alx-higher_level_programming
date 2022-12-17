3e5716fd6e1629444217c6fe7d2160799a7e82a6
ianmax32 <masagaian32@gmail.com>
2022-12-16 17:34:56 -0800

    Post an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,5 +11,5 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    r = requests.post(argv[1], email={'email': argv[2]}).text
+    r = requests.post(argv[1], {'email': argv[2]}).text
     print(r)
