d94b6d21c09159107d60702650259405efa11f9e
ianmax32 <masagaian32@gmail.com>
2022-12-16 17:39:45 -0800

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,18 @@
-#!/usr/bin/python3\n# this script
+#!/usr/bin/python3
+"""
+this script takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8).
+"""
+
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        req = requests.get(argv[1])
+        req.raise_for_status()
+        print(req.text))
+    except:
+        print('Error code: {}'.format(req.status_code))
